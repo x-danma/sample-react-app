d73a5b4a51183d5ea93b09c1ff98bdabe8335d3d
Daniel Mårtensson <daniel.martensson89@outlook.com>
2023-11-11 01:29:05 +0000

    allow CORS

--- src/sample-dotnet-api/Program.cs
+++ src/sample-dotnet-api/Program.cs
@@ -1,23 +1,47 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS services
+var  myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.SetIsOriginAllowed(_ => true)
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // log app.Environment
+    Console.WriteLine($"Environment: {app.Environment.EnvironmentName}");
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+
 }
+app.UseRouting();
+
+// Allow all CORS in development
+// UseCors should be called before UseHttpsRedirection and UseAuthorization
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
